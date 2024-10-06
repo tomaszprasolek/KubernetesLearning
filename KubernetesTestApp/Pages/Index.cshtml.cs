e018143915668c5be8b4ed630b757be64e2b8ef4
Tomasz Prasołek <tomasz.prasolek@gmail.com>
2024-10-06 14:05:44 +0200

    Move getting data from DB to another page (not index.html)

--- KubernetesTestApp/Pages/Index.cshtml.cs
+++ KubernetesTestApp/Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using KubernetesTestApp.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,21 +6,16 @@ namespace KubernetesTestApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private readonly AppDbContext _dbContext;
-
-    [BindProperty]
-    public string ProfileInfo { get; set; } = "no data";
-
+    
     [BindProperty]
     public string EnvironmentName { get; set; } = "no data";
 
     [BindProperty]
     public string AspnetcoreEnvironment { get; set; } = "no data";
 
-    public IndexModel(ILogger<IndexModel> logger, AppDbContext dbContext)
+    public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
-        _dbContext = dbContext;
     }
 
     public void OnGet()
@@ -29,27 +23,5 @@ public class IndexModel : PageModel
         EnvironmentName = Environment.GetEnvironmentVariable("HOST_NAME") ?? "no data [HOST_NAME]";
         AspnetcoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "no data [ASPNETCORE_ENVIRONMENT]";
         
-        ShowProfileInfo();
-    }
-
-    private void ShowProfileInfo()
-    {
-        try
-        {
-            var profile = _dbContext.Profiles.ToList().FirstOrDefault();
-            if (profile is null)
-            {
-                ProfileInfo = "no data in database";
-            }
-            else
-            {
-                ProfileInfo = $"[{profile.Id}] {profile.FirstName} {profile.LastName}: {profile.Profession}";    
-            }
-
-        }
-        catch (Exception e)
-        {
-            ProfileInfo = $"[ERROR] {e}";
-        }
     }
 }
--- KubernetesTestApp/Pages/Privacy.cshtml.cs
+++ KubernetesTestApp/Pages/Privacy.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KubernetesTestApp.Database;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace KubernetesTestApp.Pages;
@@ -6,13 +7,40 @@ namespace KubernetesTestApp.Pages;
 public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
+    private readonly AppDbContext _dbContext;
+    
+    [BindProperty]
+    public string ProfileInfo { get; set; } = "no data";
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public PrivacyModel(ILogger<PrivacyModel> logger, AppDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     public void OnGet()
     {
+        GetProfileInfo();
+    }
+    
+    private void GetProfileInfo()
+    {
+        try
+        {
+            var profile = _dbContext.Profiles.ToList().FirstOrDefault();
+            if (profile is null)
+            {
+                ProfileInfo = "no data in database";
+            }
+            else
+            {
+                ProfileInfo = $"[{profile.Id}] {profile.FirstName} {profile.LastName}: {profile.Profession}";    
+            }
+
+        }
+        catch (Exception e)
+        {
+            ProfileInfo = $"[ERROR] {e}";
+        }
     }
 }
