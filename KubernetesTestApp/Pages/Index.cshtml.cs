53296f7df8c81bad4e164ff83f4106be0f465487
Tomasz Prasołek <tomasz.prasolek@gmail.com>
2024-10-06 14:09:24 +0200

    Show connection string on home page

--- KubernetesTestApp/Pages/Index.cshtml.cs
+++ KubernetesTestApp/Pages/Index.cshtml.cs
@@ -6,22 +6,28 @@ namespace KubernetesTestApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    
+    private readonly IConfiguration _configuration;
+
     [BindProperty]
     public string EnvironmentName { get; set; } = "no data";
 
     [BindProperty]
     public string AspnetcoreEnvironment { get; set; } = "no data";
+    
+    [BindProperty]
+    public string ConnectionString { get; set; } = "no data";
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public void OnGet()
     {
         EnvironmentName = Environment.GetEnvironmentVariable("HOST_NAME") ?? "no data [HOST_NAME]";
         AspnetcoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "no data [ASPNETCORE_ENVIRONMENT]";
-        
+        ConnectionString = _configuration["ConnectionStrings:DefaultConnection"] ?? "no data [Connection string]";
+
     }
 }
